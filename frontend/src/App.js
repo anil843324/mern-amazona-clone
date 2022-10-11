15acc6704686322b3ee373a4a9964d05976bd9d8
anil843324 <anilkrsingh14200@gmail.com>
2022-10-11 22:15:42 +0530

    inital project step

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header >
+       <a  className='text-[#ffffff] font-bold' href="/">amazona</a>
+     
+      </header>
+      <main >list product</main>
+    </div>
+  );
+}
+
+export default App;
