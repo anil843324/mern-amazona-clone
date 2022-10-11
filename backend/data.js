0e11fe991709214a4e6c9d26d709f020e414a990
anil843324 <anilkrsingh14200@gmail.com>
2022-10-12 00:08:21 +0530

    created backend  with route

---
+++ backend/data.js
@@ -0,0 +1,50 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim shirt',
+      slug: 'nike-slim-shirt',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Adidas Fit shirt ',
+      slug: 'adidas-fit-shirt',
+      image: './images/p2.jpg', // 679 x 829
+      price: 220,
+      countInStock: 15,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Nike Slim Pant',
+      slug: 'nike-slim-Pant',
+      image: '/images/p3.jpg',
+      price: 500,
+      countInStock: 12,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+    {
+      name: 'Adidas Fit Pant ',
+      slug: 'adidas-fit-pant',
+      image: '/images/p4.jpg',
+      price: 300,
+      countInStock: 5,
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+  ],
+};
+
+export default data;
